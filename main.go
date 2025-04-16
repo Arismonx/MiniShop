0cdd3d41731dd129329eb54396db625d149234e6
Arismonx <arismonx@gmail.com>
2025-04-16 16:42:29 +0700

    add configuration management and environment setup for the application

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
 
+	"github.com/Arismonx/MiniShop/config"
+)
+
+func main() {
+	ctx := context.Background()
+	_ = ctx
+
+	//initialize config
+
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is required")
+		}
+
+		return os.Args[1]
+	}())
+
+	log.Println(cfg)
 }
