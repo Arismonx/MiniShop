a1529d4ed4a2212c99f825c9ecbc8366e45e274c
Arismonx <arismonx@gmail.com>
2025-04-27 20:30:10 +0700

    feat: update NewItemHttpHandler constructor to include config dependency

--- modules/item/itemHandler/itemHttphandler.go
+++ modules/item/itemHandler/itemHttphandler.go
@@ -1,19 +1,24 @@
 package itemHandler
 
-import _itemUsecase "github.com/Arismonx/MiniShop/modules/item/itemUsecase"
+import (
+	"github.com/Arismonx/MiniShop/config"
+	_itemUsecase "github.com/Arismonx/MiniShop/modules/item/itemUsecase"
+)
 
 // Create interface and struct
 type (
 	ItemHttpHandlerService interface{}
 
 	itemHttpHandler struct {
+		cfg         *config.Config
 		itemUsecase _itemUsecase.ItemUsecaseService
 	}
 )
 
 // Create constructor
 func NewItemHttpHandler(
+	cfg *config.Config,
 	itemUsecase _itemUsecase.ItemUsecaseService,
 ) ItemHttpHandlerService {
-	return &itemHttpHandler{itemUsecase}
+	return &itemHttpHandler{cfg, itemUsecase}
 }
