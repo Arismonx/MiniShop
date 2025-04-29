10e916ea90c4a01d12696b75bf3eff806447ba6d
Arismonx <arismonx@gmail.com>
2025-04-29 08:34:23 +0700

    feat: implement response package with success and error response functions

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,1 +1,20 @@
 package response
+
+import "github.com/labstack/echo/v4"
+
+//Create package response
+type (
+	MsgResponse struct {
+		Message string `json:"message"`
+	}
+)
+
+//function response successful
+func SuccessResponse(ctx echo.Context, statusCode int, data any) error {
+	return ctx.JSON(statusCode, data)
+}
+
+//function response error
+func ErrResponse(ctx echo.Context, statusCode int, message string) error {
+	return ctx.JSON(statusCode, &MsgResponse{Message: message})
+}
