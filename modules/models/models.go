8be0f104d31113cf3742a3a0719825507dd17c1b
Arismonx <arismonx@gmail.com>
2025-05-01 21:35:45 +0700

    feat: add pagination and Kafka offset models

--- modules/models/models.go
+++ modules/models/models.go
@@ -1,1 +1,29 @@
 package models
+
+type (
+	PaginateReq struct {
+		Start string `query:"start" validate:"max=64"`
+		Limit int    `query:"limit" validate:"required,min=2,max=10"`
+	}
+
+	PaginateRes struct {
+		Date  any           `json:"date"`
+		Limit int           `json:"limit"`
+		Total int64         `json:"total"`
+		First FirstPaginate `json:"first"`
+		Next  NextPaginate  `json:"next"`
+	}
+
+	FirstPaginate struct {
+		Href  string `json:"href"`
+		Start string `jsin:"start"`
+	}
+
+	NextPaginate struct {
+		Href string `json:"href"`
+	}
+
+	KafkaOffset struct {
+		Offset int64 `json:"offset" bson:"offset"`
+	}
+)
